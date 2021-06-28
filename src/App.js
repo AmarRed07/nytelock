3b12fdeccd958535317cab351a41bc119eb939aa
AmarRed <amarredzepagic@MacBook-Air.local>
2021-06-27 20:08:14 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Particles from 'react-particles-js'
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+
 
 function App() {
   return (
-
+    <>
+    <Particles
+      params= {{
+        particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area:900
+            }
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 3,
+              color: "#6f6f6f"
+            }
+          }
+        }
+      }}
+     />
     <Navbar />
+    <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+      <div className="header-wraper">
+          <div className="main-info">
+              <h6 className="sub-heading">
+              <ul>
+                  <li>
+                        <span>What services do we offer?</span>
+                  </li>
+              </ul>
+              </h6>
+              <h2 className="header-h2">
+                  Stand Out From The Rest With Professional Branding
+              </h2>
+              </div>
+              <a href="#" className="btn-main-offer">Get a Quote </a>
+          </div>
+    )
+}
+
+export default Header
